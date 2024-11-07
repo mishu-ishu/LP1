ea53e38239a5ee57fee4064c280c426496370652
mishu-ishu <143538433+mishu-ishu@users.noreply.github.com>
2024-11-07 22:44:09 +0530

    Delete SPOS/MP pass 1 directory

--- SPOS/MP pass 1/macro pass 1/Obj.java
+++
@@ -1,9 +0,0 @@
-public class Obj {
-    public String name;
-    public int addr;
-
-    public Obj(String name, int addr) {
-        this.name = name;
-        this.addr = addr;
-    }
-}
--- SPOS/MP pass 1/macro pass 1/Pooltable.java
+++
@@ -1,9 +0,0 @@
-public class Pooltable {
-    public int first;
-    public int total_literals;
-
-    public Pooltable(int first, int total_literals) {
-        this.first = first;
-        this.total_literals = total_literals;
-    }
-}
